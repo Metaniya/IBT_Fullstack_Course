80572b1e71df1913471606faeb2175362f298e96
Metaniya Shiferaw <125639579+Metaniya@users.noreply.github.com>
2026-09-04 22:55:24 +0300

    Add day 2 and day 3

---
+++ Module-3/Day2/Day2-exersise/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header.jsx";
+import Menu from "./Menu.jsx";
+import "./index.css";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Menu />
+        </>
+    );
+}
+
+export default App;
